34785f9d3db1dbeecc841a6552980e06bfc7f1ff
Eric Evans <eevans@wikimedia.org>
2019-12-20 15:48:04 -0600

    Turn Datasource into an interface

---
+++ src/main/java/org/wikimedia/covenant/CassandraDatasource.java
@@ -0,0 +1,39 @@
+package org.wikimedia.covenant;
+
+public class CassandraDatasource implements Datasource {
+	public void start() {
+
+	}
+
+	public Record<?> read(String key) {
+		return null;
+	}
+
+	public void write(String key, Record<?> record) {
+
+	}
+
+	public void prev(String key, Record<?> record) {
+
+	}
+
+	public void delete(String key) {
+
+	}
+
+	public void prepare(String key, Record<?> record) {
+
+	}
+
+	public void commit(String key, Record<?> record) {
+
+	}
+
+	public void abort(String key, Record<?> record) {
+
+	}
+
+	public void recover(String key) {
+
+	}
+}
--- src/main/java/org/wikimedia/covenant/Datasource.java
+++ src/main/java/org/wikimedia/covenant/Datasource.java
@@ -1,39 +1,13 @@
 package org.wikimedia.covenant;
 
-public class Datasource {
-	public void start() {
-
-	}
-
-	public Record<?> read(String key) {
-		return null;
-	}
-
-	public void write(String key, Record<?> record) {
-
-	}
-
-	public void prev(String key, Record<?> record) {
-
-	}
-
-	public void delete(String key) {
-
-	}
-
-	public void prepare(String key, Record<?> record) {
-
-	}
-
-	public void commit(String key, Record<?> record) {
-
-	}
-
-	public void abort(String key, Record<?> record) {
-
-	}
-
-	public void recover(String key) {
-
-	}
+public interface Datasource {
+	public void start();
+	public Record<?> read(String key);
+	public void write(String key, Record<?> record);
+	public void prev(String key, Record<?> record);
+	public void delete(String key);
+	public void prepare(String key, Record<?> record);
+	public void commit(String key, Record<?> record);
+	public void abort(String key, Record<?> record);
+	public void recover(String key);
 }
