235198ed313130b5f5e3870b6b73171fea4e7aec
Eric Evans <eevans@wikimedia.org>
2019-12-23 16:01:12 -0600

    add missing attributes

--- src/main/java/org/wikimedia/covenant/Record.java
+++ src/main/java/org/wikimedia/covenant/Record.java
@@ -1,9 +1,21 @@
 package org.wikimedia.covenant;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class Record<T> {
+	// TABLE I: Record structure
+	Date startTime;
+	Date endTime;
+	Date leaseTime;
+	UUID txID;
+	byte state;
+	Date updateTime;
+	int version;
+	int previousVersion;
+
 	private Map<String, T> record = new HashMap<String, T>();
 
 	public T get(String column) {
